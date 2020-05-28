259e67175139b118758e24b5e27b486739900783
ranganathprn <ranganathprn@gmail.com>
2020-05-28 10:39:25 +0530

    fixing package structure

--- zieta/src/main/java/com/zietaproj/zieta/dto/ProjectMasterDTO.java
+++ zieta/src/main/java/com/zietaproj/zieta/dto/ProjectMasterDTO.java
@@ -1,14 +1,9 @@
 package com.zietaproj.zieta.dto;
 
-import java.util.Date;
-import javax.persistence.Column;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotBlank;
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 public class ProjectMasterDTO {
 	private Long id;
     private Long client_id;
@@ -16,42 +11,7 @@ public class ProjectMasterDTO {
     private String created_by;
     private String modified_by;
     private boolean IS_DELETE;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public Long getClient_id() {
-		return client_id;
-	}
-	public void setClient_id(Long client_id) {
-		this.client_id = client_id;
-	}
-	public String getProject_type() {
-		return project_type;
-	}
-	public void setProject_type(String project_type) {
-		this.project_type = project_type;
-	}
-	public String getCreated_by() {
-		return created_by;
-	}
-	public void setCreated_by(String created_by) {
-		this.created_by = created_by;
-	}
-	public String getModified_by() {
-		return modified_by;
-	}
-	public void setModified_by(String modified_by) {
-		this.modified_by = modified_by;
-	}
-	public boolean isIS_DELETE() {
-		return IS_DELETE;
-	}
-	public void setIS_DELETE(boolean iS_DELETE) {
-		IS_DELETE = iS_DELETE;
-	}
-    
+    private String client_code;
+    private String project_code;
     
 }
--- zieta/src/main/java/com/zietaproj/zieta/dto/TaskMasterDTO.java
+++ zieta/src/main/java/com/zietaproj/zieta/dto/TaskMasterDTO.java
@@ -10,6 +10,11 @@ import javax.validation.constraints.NotBlank;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TaskMasterDTO {
 	private Long id;
     private Long client_id;
@@ -17,44 +22,7 @@ public class TaskMasterDTO {
     private String modified_by;
     private String created_by;
     private boolean IS_DELETE;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public Long getClient_id() {
-		return client_id;
-	}
-	public void setClient_id(Long client_id) {
-		this.client_id = client_id;
-	}
-	public String getTask_type() {
-		return task_type;
-	}
-	public void setTask_type(String task_type) {
-		this.task_type = task_type;
-	}
-	
-	public boolean isIS_DELETE() {
-		return IS_DELETE;
-	}
-	public void setIS_DELETE(boolean iS_DELETE) {
-		IS_DELETE = iS_DELETE;
-	}
-	public String getModified_by() {
-		return modified_by;
-	}
-	public void setModified_by(String modified_by) {
-		this.modified_by = modified_by;
-	}
-	public String getCreated_by() {
-		return created_by;
-	}
-	public void setCreated_by(String created_by) {
-		this.created_by = created_by;
-	}
-	
-	
+    private String client_code;
+    private String project_code;
 	
 }

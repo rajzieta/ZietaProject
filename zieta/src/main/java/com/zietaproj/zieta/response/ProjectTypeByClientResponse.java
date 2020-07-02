71b377b49195628b1cc11a003060273829d3d578
Zietaaccount <reena.thanmai@zietatech.com>
2020-07-02 22:16:02 +0530

    Reena8 (#60)
    * Refactor into camelcase for customer Information and Message Details API
    
    * add ttimeentry and getProjectTypeByClient
    
    * Merge branch 'master' into reena8
    
    * checking null incase of userid as null
    
    Co-authored-by: ranganathprn <ranganathprn@gmail.com>

---
+++ zieta/src/main/java/com/zietaproj/zieta/response/ProjectTypeByClientResponse.java
@@ -0,0 +1,18 @@
+package com.zietaproj.zieta.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProjectTypeByClientResponse {
+
+	private Long projectTypeId;
+    private Long clientId;
+    private String projectType;
+    private String createdBy;
+    private String modifiedBy;
+    private boolean isDELETE;
+	
+	
+}

9242aae1692071a2916555ceb65fa004fcea51c7
Zietaaccount <reena.thanmai@zietatech.com>
2020-10-16 06:55:50 +0000

    enable filter (#172)
    * including projectId attribute, it got missed in the earlier code
    
    * including expense related workflow
    
    * enabling filter
    
    * enable filter
    
    Co-authored-by: ranganathprn <ranganathprn@gmail.com>

--- zieta/src/main/java/com/zieta/tms/filter/WhiteListIpFilter.java
+++ zieta/src/main/java/com/zieta/tms/filter/WhiteListIpFilter.java
@@ -15,7 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-//@Component
+@Component
 public class WhiteListIpFilter extends OncePerRequestFilter {
 
 	@Value("#{'${whitelist.ipaddress}'.split(',')}")

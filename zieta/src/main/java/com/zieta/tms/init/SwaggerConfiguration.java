725ac529d408f31cfa86b04758ffbf5dd6093acf
ranganathprn <ranganathprn@gmail.com>
2021-02-05 13:55:43 +0530

    disabling swagger for prod profile (#273)
    * allowing submitdate to be populated only during submit ts action.
    
    allowing submitdate to be populated only during submit ts action.
    
    * considering the approver id as reporting manager id when the userid is same as approverid
    
    considering the approver id as reporting manager id when the userid is same as approverid
    
    * changing the status for tsentries when templateid is 1
    
    * Using SXSSF  to improve performance
    
    * performance fix for the detailed timesheet
    
    * changing the attachments name for timesheet details and summary
    
    * setting the auto column size of the cells
    
    * rolling back the setting auto column size, as its degrading the performance
    
    * removing auto size from project reports
    
    * getAllExpensesHistoryByClientUser filtered ther results other than Draft status
    
    * refactoring the key attribute
    
    * changing the key name to multipartFile-data
    
    * updating the file details during the expense attachment upload
    
    * switching to submiters RM, when submiter and approver is same
    
    * fixing the attachment path
    
    * getAllExpensesHistoryByClientUser included filter based on exp start date
    
    * leave report generation with pagination
    
    * leave report with pagination and download, filter with clientid ,start date and enddate
    
    leave report with pagination and download, filter with clientid ,start date and enddate
    
    * getWorkFlowRequestsByApprover modified the filter based on the ts_date
    
    * getWorkFlowRequestsByApprover api modified the filter based on the ts_date
    
    * getWorkFlowRequestsByApprover api modified the filter based on the ts_date
    
    getWorkFlowRequestsByApprover api modified the filter based on the ts_date
    
    * getWorkFlowRequestsByApprover api modified the filter based on the ts_da…
    
    * adding leave id to the report
    
    * Changing to getExpAmtInr instead getExpAmount
    
    * enabling profile
    
    * Included Expense report and getLeaveHistoryByApprover
    
    * Included Expense report and getLeaveHistoryByApprover
    
    * Fix for the issues 536,609,620,622
    
    Fix for the issues 536,609,620,622
    
    * Incorporated changes for 538, 608 and added emp_id for the expense_summary
    
    * included activity status
    
    * included activity status
    
    * Provided pagination for Leave and TS reports
    
    * reverting the  TS pagination logic
    
    * disabling swagger for prod profile

--- zieta/src/main/java/com/zieta/tms/init/SwaggerConfiguration.java
+++ zieta/src/main/java/com/zieta/tms/init/SwaggerConfiguration.java
@@ -2,6 +2,7 @@ package com.zieta.tms.init;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,6 +12,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@Profile({"!prod"})
 @EnableSwagger2
 public class SwaggerConfiguration {                                    
     @Bean

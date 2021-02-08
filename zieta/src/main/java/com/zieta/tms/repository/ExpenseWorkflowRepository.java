21f0e8bc73c289757e36f6103926cf789dc2f5b0
ranganathprn <ranganathprn@gmail.com>
2021-02-09 00:48:45 +0530

    getExpenseWorkFlowRequestsHistoryByApprover filter based on expense start date

--- zieta/src/main/java/com/zieta/tms/repository/ExpenseWorkflowRepository.java
+++ zieta/src/main/java/com/zieta/tms/repository/ExpenseWorkflowRepository.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.zieta.tms.model.ExpenseWorkflowRequest;
@@ -16,11 +17,9 @@ public interface ExpenseWorkflowRepository extends JpaRepository<ExpenseWorkflow
 
 	List<ExpenseWorkflowRequest> findByApproverIdAndActionTypeNotIn(long approverId, Collection<Long> actionType);
 
-	//List<ExpenseWorkflowRequest> findByApproverIdAndActionDateBetweenAndActionTypeIn(long approverId,
-	//		Date startActiondate, Date endActionDate, Collection<Long> actionType);
-	
 	ExpenseWorkflowRequest findByExpId(long expId);
-
+	
+	@Query(value ="SELECT * FROM expwf_request ewfr, expense_info einfo WHERE einfo.ID=ewfr.EXP_ID AND ewfr.APPROVER_ID=?1 AND einfo.EXP_START_DATE >=?2 AND einfo.EXP_START_DATE <=?3 AND ewfr.ACTION_TYPE IN ?4", nativeQuery=true)
 	List<ExpenseWorkflowRequest> findByApproverIdAndRequestDateBetweenAndActionTypeIn(long approverId, Date startRequestDate,
 			Date endRequestDate, Collection<Long> actionType);
 

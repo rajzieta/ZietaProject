d603b6d6717ccddd015aa289779e96e53c2c0c26
Rajm2 <81280821+rajzieta@users.noreply.github.com>
2021-07-26 14:33:08 +0530

    update for connection master management

---
+++ zieta/src/main/java/com/zieta/tms/repository/ConnectionMasterInfoRepository.java
@@ -0,0 +1,38 @@
+package com.zieta.tms.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.zieta.tms.model.ConnectionMasterInfo;
+import com.zieta.tms.model.OrgUnitUserMapping;
+import com.zieta.tms.model.UserInfo;
+
+@Repository
+public interface ConnectionMasterInfoRepository extends JpaRepository<ConnectionMasterInfo, Long>{
+	
+	//ConnectionMasterInfo findByEmail(String email);
+	
+	//ConnectionMasterInfo findByClientAndIsDelete(Long clientId, short notDeleted);
+	
+	List<ConnectionMasterInfo> findByClientIdAndIsDelete(Long client_id, short notDeleted);
+
+	List<ConnectionMasterInfo> findByIsDelete(short notDeleted);
+
+	List<Long> findByClientId(Long clientId);
+
+	/*@Query(value="select * from user_info where id=?1", nativeQuery=true)
+	UserInfo findByUserId(long userId);
+	
+	UserInfo findByExpTemplateId(Long expTemplateId);*/
+
+	
+
+
+	
+	
+
+}

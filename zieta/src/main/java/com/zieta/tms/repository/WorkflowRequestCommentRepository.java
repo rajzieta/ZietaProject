5e9d640a029712be37816576bfdfd605c5678868
Rajm2 <81280821+rajzieta@users.noreply.github.com>
2021-12-07 15:49:57 +0530

    update workfollow request

--- zieta/src/main/java/com/zieta/tms/repository/WorkflowRequestCommentRepository.java
+++ zieta/src/main/java/com/zieta/tms/repository/WorkflowRequestCommentRepository.java
@@ -3,6 +3,7 @@ package com.zieta.tms.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.zieta.tms.model.WorkFlowRequestComments;
@@ -12,6 +13,8 @@ public interface WorkflowRequestCommentRepository extends JpaRepository<WorkFlow
 	
 	public List<WorkFlowRequestComments> findByTsIdOrderByIdDesc(long tsId);
 	
+	@Query("select wfrc from WorkFlowRequestComments wfrc where wfrc.tsId =?1 ORDER BY wfrc.id DESC")
+	public List<WorkFlowRequestComments> findByTsIdDesc(long tsId);
 	
 
 }
